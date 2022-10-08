b5aafe2b10e3c1763f33f86676a868e4540672b1
windoxas <xurshidbekjon@mail.ru>
2022-10-08 13:51:27 +0300

    some change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import './App.css';
-import Header from './components/Header/Header'
-import Movies from './components/Movies/Movies'
 
-
+import React from 'react'
+import './App.scss';
+import axios  from 'axios';
 
 
 function App() {
-  const hi = "Salom mening dustlarim"
+
+  const [value , setValue] = React.useState('')
+  const [todo, setTodo] = React.useState([])
+  const [remove, setRemove] = React.useState(0)
+  function getValue(e){
+    setValue(e.target.value)
+  }
+
+  React.useEffect(() => {
+    async function getTodoData(){
+        let todoData = await axios.get('http://localhost:3004/todo')
+        setTodo(todoData.data)
+    }
+    getTodoData()
+  }, [value, remove])
+
+
+ async function postTodo() {
+
+  if(todo.find(item => item.todoName === value)){
+    return alert('this todo already add to todo list')
+  }else{
+    if(value === ""){
+      return undefined
+     }else{
+      await axios.post('http://localhost:3004/todo',{
+        "todoName": value
+      })
+     }
+  }
+
+     
+    
+      setValue("")
+  }
+
+
+ async function deleteTodo(id){
+      await axios.delete(`http://localhost:3004/todo/${id}`)
+      setRemove(id)
+  }
+
+
+
+
   return (
     <div className="App">
-        <Header hello={hi}/>
-        <Movies title="1 page" page={1}/>
-        <Movies title="2 page" page={2}/>
-        <Movies title="3 page" page={3}/>
-        <Movies title="4 page" page={4}/>
-        <Movies title="5 page" page={5}/>
-        <Movies title="6 page" page={6}/>
+
+      <div className="form">
+          <input onInput={(e) => getValue(e)} placeholder="add todo"  value={value}/>
+          <button onClick={() => postTodo()}>Send</button>
+      </div>
+
+      <div className="listTodo">
+          <ul>
+              {todo.map((item,index) => {
+                  return  <li 
+                  key={item.id}>
+                    {`${index+1}  ${item.todoName}`}
+                    <img onClick={() => deleteTodo(item.id)} src="https://cdn-icons-png.flaticon.com/512/1632/1632602.png" alt="" />
+                     </li>
+              })}
+          </ul>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -4,19 +4,19 @@ import axios from 'axios'
 
 
 
-export default function Movies(props) {
+export default function Movies({title, page}) {
 
     const [movies , setMovies] = React.useState([])
 
 
     React.useEffect(() => {
        async function getMovies(){
-            const data = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=60413a5f672cfb8007082bc512040ca2&language=en-US&page=${props.page}`)
+            const data = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=60413a5f672cfb8007082bc512040ca2&language=en-US&page=${page}`)
             setMovies(data.data.results)
         }
 
          getMovies()
-    },[])
+    },[page])
 
    
 
@@ -24,7 +24,7 @@ export default function Movies(props) {
     <div className='movies'>
         
         <div className="moviesBlock">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="scroll_movies">
                 {movies.map(item =>{
                     return <img key={item.id} src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt="" />
